937e1725494cdc63956b6fe729ddb3da3441b009
Antonio Molinari <molinari@incode.it>
2024-05-24 15:47:27 +0200

    testing flipbook

--- src/views/FlipbookView.vue
+++ src/views/FlipbookView.vue
@@ -1,9 +1,20 @@
 <script setup>
+import Flipbook from 'flipbook-vue'
+const pages = Array(10).fill().map((_, i) => `https://picsum.photos/500/800?v=${i}`)
+console.log(pages)
 </script>
 
 <template>
-  Flipbook view
+  <flipbook 
+    class="flipbook" 
+    :pages="pages">
+  </flipbook>
 </template>
 
 <style lang="scss">
+.flipbook {
+  // border: 1px solid black;
+  width: 80vw;
+  height: 800px;
+}
 </style>
